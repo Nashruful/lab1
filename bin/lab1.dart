33866786394d5e04c7c841e611dfbbf0f9df278d
Nashruful <azooz.n40@hotmail.com>
2024-07-31 11:26:15 +0300

    second edit

--- bin/lab1.dart
+++ bin/lab1.dart
@@ -1,5 +1,7 @@
-import 'package:lab1/lab1.dart' as lab1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${lab1.calculate()}!');
+  print(hello(message: "Hi Abdulaziz"));
+}
+hello({required String message}){
+  return message;
 }
