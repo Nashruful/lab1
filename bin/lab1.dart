1c9c8cbbb5542ff71bbdffa84304cdedc462b2c6
Nashruful <azooz.n40@hotmail.com>
2024-07-31 11:39:38 +0300

    added new branch b1

--- bin/lab1.dart
+++ bin/lab1.dart
@@ -1,7 +1,12 @@
-
 void main(List<String> arguments) {
   print(hello(message: "Hi Abdulaziz"));
+  hellob1(message: "Hi branch 1");
 }
-hello({required String message}){
+
+hello({required String message}) {
   return message;
+}
+
+hellob1({required String message}) {
+  print(message);
 }
